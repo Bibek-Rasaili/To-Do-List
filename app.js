13b21b2cb329de4228fb2d4b019c7c1e6facba8f
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-20 04:37:59 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs'); //use ejs as view engine
+
+app.get("/", function(req, res){
+  let today = new Date();
+
+  let options = {
+    weekday : "long",
+    day : "numeric",
+    month : "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {kindOfDay : day, newListItems : items});
+});
+
+
+app.post("/", function(req, res){
+   items.push(req.body.newItem);
+
+  res.redirect("/");
+});
+
+
+app.listen(3000, function(){
+  console.log("Server is listening on port 3000");
+})

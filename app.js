8f45024d9e5aff8550ea2c85c2091446dc7a5de8
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-20 05:10:52 +0000

    dynamically handles the list.ejs to display/render 2 different webpage - / (Home route) or /work (work route).

--- app.js
+++ app.js
@@ -4,35 +4,64 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs'); //use ejs as view engine
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   let today = new Date();
 
   let options = {
-    weekday : "long",
-    day : "numeric",
-    month : "long"
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   };
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay : day, newListItems : items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 });
 
 
-app.post("/", function(req, res){
-   items.push(req.body.newItem);
+app.post("/", function(req, res) {
+  if (req.body.list === "Work") {
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.newItem);
 
-  res.redirect("/");
+    res.redirect("/");
+  }
 });
 
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
 
-app.listen(3000, function(){
+// *****
+// This is unnecessary because the form from which post is called is directed at "/"
+// Therefore a logic has been performed in app.post("/", ..) to determine/differentiate,
+// If the new item is for / (homepage) or /work (work) page
+// ________________
+// app.post("/work", function(req, res) {
+//   let items = req.body.newItem;
+//   workItems.push(items);
+//
+//   res.redirect("/work");
+// });
+
+app.listen(3000, function() {
   console.log("Server is listening on port 3000");
 })
